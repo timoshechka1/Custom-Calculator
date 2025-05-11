ffca0e5d2f23c646188daa8c55f4c1be87862acb
timoshechka1 <ttimoshechka@gmail.com>
2025-05-11 20:12:17 +0300

    feat: Сode refactoring, moving label text to the bottom right corner

--- main.py
+++ main.py
@@ -1,46 +1,32 @@
 from kivy.app import App
+from kivy.config import Config
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 
+Config.set("graphics", "resizable", 1)
+Config.set("graphics", "width", 400)
+Config.set("graphics", "height", 500)
+
 class CustomCalculatorApp(App):
     def build(self):
-        boxlay = BoxLayout(orientation = "vertical")
-        gridlay = GridLayout(cols = 4)
+        boxlay = BoxLayout(orientation = "vertical", padding = 10)
+        gridlay = GridLayout(cols = 4, spacing = 3, size_hint = (1, 0.6), )
 
-        boxlay.add_widget(Label(text="0"))
+        boxlay.add_widget(Label(text="0", font_size = 40, halign = "right", valign = "bottom",
+                                size_hint = (1, 0.4), text_size = (400 - 20, 500 * 0.4 - 20)))
 
-        gridlay.add_widget(Button(text="%"))
-        gridlay.add_widget(Button(text="√"))
-        gridlay.add_widget(Button(text="x²"))
-        gridlay.add_widget(Button(text="¹/x"))
-
-        gridlay.add_widget(Button(text="CE"))
-        gridlay.add_widget(Button(text="C"))
-        gridlay.add_widget(Button(text="⌫"))
-        gridlay.add_widget(Button(text="÷"))
-
-        gridlay.add_widget(Button(text="7"))
-        gridlay.add_widget(Button(text="8"))
-        gridlay.add_widget(Button(text="9"))
-        gridlay.add_widget(Button(text="×"))
-
-        gridlay.add_widget(Button(text="4"))
-        gridlay.add_widget(Button(text="5"))
-        gridlay.add_widget(Button(text="6"))
-        gridlay.add_widget(Button(text="-"))
-
-        gridlay.add_widget(Button(text="1"))
-        gridlay.add_widget(Button(text="2"))
-        gridlay.add_widget(Button(text="3"))
-        gridlay.add_widget(Button(text="+"))
-
-        gridlay.add_widget(Button(text="±"))
-        gridlay.add_widget(Button(text="0"))
-        gridlay.add_widget(Button(text="."))
-        gridlay.add_widget(Button(text="="))
+        buttons = [ "%", "√", "x²", "¹/x",
+                    "CE", "C", "⌫", "÷",
+                    "7", "8", "9", "×",
+                    "4", "5", "6", "-",
+                    "1", "2", "3", "+",
+                    "±", "0", ".", "="
+                    ]
+        for btn in buttons:
+            gridlay.add_widget(Button(text=btn))
 
         boxlay.add_widget(gridlay)
         return boxlay
